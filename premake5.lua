1c7692fe373f0e96bc5bc1d307a0db49d0fcc485
dstacer <dstacer@gmail.com>
2021-02-12 19:45:11 -0600

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,26 @@
-project "imgui"
+project "yaml"
    kind "StaticLib"
    staticruntime "on"
    language "C++"
    cppdialect "C++17"
    targetdir ("bin/" .. outputdir)
    objdir ("Intermediate/" .. outputdir)
-   defines { "IMGUI_IMPL_OPENGL_LOADER_GLAD" }
+   
 
    files 
    { 
-		"imconfig.h", 
-		"imgui.h",
-        "imgui.cpp",
-        "imgui_draw.cpp", 
-		"imgui_internal.h",
-        "imgui_widgets.cpp",
-        "imgui_tables.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
    }
 
    filter "system:windows"
